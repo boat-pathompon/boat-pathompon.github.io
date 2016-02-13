23236cadaa1525934439668f214be4f6d67788f2
Boat Pathompon <boat@eventpop.me>
2016-02-13 17:31:15 +0700

    Remove old code

--- Gemfile
+++ Gemfile
@@ -2,20 +2,14 @@
 # the following line to use "http://" instead
 source 'https://rubygems.org'
 
-ruby '2.1.3'
+ruby '2.2.3'
 
 gem "middleman"
-gem "middleman-blog"
 
 gem "coffee-script"
 gem "sass"
 gem "font-awesome-sass"
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'bootstrap', '~> 4.0.0.alpha3'
 
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,77 +1,8 @@
-# Blog
-Time.zone = "Asia/Bangkok"
-
-activate :blog do |blog|
-  blog.permalink = "posts/{title}.html"
-  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
-  blog.taglink = "tags/{tag}.html"
-  blog.default_extension = ".erb"
-
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
-
-  blog.paginate = false
-end
-
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
-
-
 # Helpers
 helpers do
-  def post_time(article)
-    time_text = [article.date.strftime('%Y-%m-%d'), article.data.time]
-    content_tag :div, class: 'post-time' do
-      [
-        content_tag(:i, '', class: 'fa fa-clock-o'),
-        content_tag(:abbr, time_text.join(' '), title: time_text.join('T'))
-      ].join.html_safe
-    end
-  end
-
-  def post_tags(article)
-    content_tag :div, class: 'post-tags' do
-      [
-        content_tag(:i, '', class: 'fa fa-tags'),
-        article.tags.map { |tag| link_to(tag, '#') }.join(', ').html_safe
-        # article.tags.map { |tag| link_to(tag, tag_path(tag)) }.join(', ').html_safe
-      ].join.html_safe
-    end
-  end
-
-  def share_btn(article, text, provider = :facebook)
-    params = {
-      u: URI.escape(URI.join('http://littleb.land', article.url).to_s)
-    }
-
-    share_url = case provider
-      when :facebook then 'http://facebook.com/sharer.php'
-    end
-
-    link_to text,
-      "#{share_url}?#{params.to_param}",
-      target: '_blank',
-      class: provider
-  end
-
-  def facebook_og_meta(object, type)
-    url = URI.join('http://littleb.land', object.url).to_s
-    tags = [
-      content_tag(:meta, '', property: 'og:title', content: object.data.title),
-      content_tag(:meta, '', property: 'og:url', content: url),
-    ]
-
-    if type == :article
-      tags << content_tag(:meta, '', property: 'og:image', content: object.data.cover)
-    end
-
-    tags.join
-  end
 end
 
 # Autoprefixer
